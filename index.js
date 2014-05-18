2378a572cfa3c708da4801ad5cf05581b4a0827f
kaelzhang <i@kael.me>
2014-05-18 20:10:10 +0800

    fixes the bug that quoted arguments will fail; fixes #4

--- index.js
+++ index.js
@@ -1,38 +1,39 @@
 'use strict';
 
-var spawn   = require('child_process').spawn;
-var EE      = require('events').EventEmitter;
-var util    = require('util');
-var async   = require('async');
+var spawn = require('child_process').spawn;
+var EE = require('events').EventEmitter;
+var util = require('util');
+var async = require('async');
 
 module.exports = spawns;
 
-function spawns (commands, options){
-    return new Spawns(commands, options);
+function spawns(commands, options) {
+  return new Spawns(commands, options);
 }
+spawns.Spawns = Spawns;
 
 // @param {Array.<string>} commands 
-function Spawns (commands, options){
-    if ( !util.isArray(commands) ) {
-        commands = [commands];
-    }
+function Spawns(commands, options) {
+  if (!util.isArray(commands)) {
+    commands = [commands];
+  }
 
-    // [
-    //     // single command
-    //     [command],
+  // [
+  //     // single command
+  //     [command],
 
-    //     // piped commands
-    //     [command, command]
-    // ]
-    this._commands = this._parse_commands(commands);
-    this._options = options;
-    var self = this;
+  //     // piped commands
+  //     [command, command]
+  // ]
+  this._commands = this._parse_commands(commands);
+  this._options = options;
+  var self = this;
 
-    // run in the next tick, 
-    // so that forward event buidings could be applied
-    process.nextTick(function () {
-        self._process(); 
-    });
+  // run in the next tick, 
+  // so that forward event buidings could be applied
+  process.nextTick(function() {
+    self._process();
+  });
 }
 
 util.inherits(Spawns, EE);
@@ -47,19 +48,19 @@ util.inherits(Spawns, EE);
 // this.emit('child_exit');
 
 Spawns.prototype._process = function() {
-    var self = this;
-    async.series(this._commands.map(function (command) {
-        return function(done){
-            if ( command.length === 1 ) {
-                self._spawn(command[0], done);
-            }
-        };
-        
-    }), function (code, status) {
-        var last = status.pop();
+  var self = this;
+  async.series(this._commands.map(function(command) {
+    return function(done) {
+      if (command.length === 1) {
+        self._spawn(command[0], done);
+      }
+    };
 
-        self.emit('close', last[0], last[1]);
-    });
+  }), function(code, status) {
+    var last = status.pop();
+
+    self.emit('close', last[0], last[1]);
+  });
 };
 
 
@@ -69,57 +70,109 @@ Spawns.prototype._process = function() {
 //      args:     
 // }
 Spawns.prototype._spawn = function(command, done) {
-    this.emit('spawn', command.origin);
+  this.emit('spawn', command.origin);
 
-    var child = spawn(command.name, command.args, this._options);
-    var self = this;
+  var child = spawn(command.name, command.args, this._options);
+  var self = this;
 
-    child.on('error', function (err) {
-        self.emit('error', err);
-    });
+  child.on('error', function(err) {
+    self.emit('error', err);
+  });
 
-    child.on('exit', function (code, signal) {
-        self.emit('child_exit', code, signal);
-    });
+  child.on('exit', function(code, signal) {
+    self.emit('child_exit', code, signal);
+  });
 
-    child.on('close', function (code, signal) {
-        self.emit('child_close', code, signal);
-        done(code, code, signal);
-    });
+  child.on('close', function(code, signal) {
+    self.emit('child_close', code, signal);
+    done(code, code, signal);
+  });
 
-    return child;
+  return child;
 };
 
 
 // @param {Array.<string>} commands
 Spawns.prototype._parse_commands = function(commands) {
-    return commands.map(this._parse_piped, this);
+  return commands.map(this._parse_piped, this);
 };
 
 
-Spawns.prototype._parse_piped = function (command) {
-    return command.split('|')
-        .map(function (pipe) {
-            return pipe.trim();
-        })
-        .filter(Boolean)
-        .map(this._parse_command, this);
+Spawns.prototype._parse_piped = function(command) {
+  return command.split('|')
+    .map(function(pipe) {
+      return pipe.trim();
+    })
+    .filter(Boolean)
+    .map(this._parse_command, this);
 };
 
 
 // @param {string} command
 Spawns.prototype._parse_command = function(command) {
-    var slices = command.split(/\s+/).map(function (slice) {
-        return slice.trim();
-    });
 
-    var name = slices.shift();
+  // #4,
+  // We should reserve quoted spaces, so never use `/\s+/`
+  var slices = command.split(/\s/).map(function(slice) {
+    return slice.trim();
+  });
 
-    return {
-        name    : name,
-        args    : slices,
-        origin  : command
-    };
+  var name = slices.shift();
+
+  return {
+    name: name,
+    args: this._balance_args(slices),
+    origin: command
+  };
 };
 
 
+var DOUBLE_QUOTE_START = /^".*[^"]$/;
+var DOUBLE_QUOTE_END = /"$/;
+
+var SINGLE_QUOTE_START = /^'.*[^']$/;
+var SINGLE_QUOTE_END = /'$/;
+
+// #4,
+// Deal with quotes
+Spawns.prototype._balance_args = function(slices) {
+  var double_quoted;
+  var single_quoted;
+  var stack = [];
+  return slices.reduce(function(prev, current) {
+    if (double_quoted) {
+      stack.push(current);
+      if (DOUBLE_QUOTE_END.test(current)) {
+        double_quoted = false;
+
+        prev.push(stack.join(' '));
+        stack.length = 0;
+      }
+
+    } else if (single_quoted) {
+      stack.push(current);
+      if (SINGLE_QUOTE_END.test(current)) {
+        single_quoted = false;
+
+        prev.push(stack.join(' '));
+        stack.length = 0;
+      }
+
+    // if not quoted, and current is empty, just skip
+    } else if (current) {
+      if (DOUBLE_QUOTE_START.test(current)) {
+        stack.push(current);
+        double_quoted = true;
+
+      } else if (SINGLE_QUOTE_START.test(current)) {
+        stack.push(current);
+        single_quoted = true;
+
+      } else {
+        prev.push(current);
+      }
+    }
+
+    return prev;
+  }, []);
+};
--- test/spawns.js
+++ test/spawns.js
@@ -3,15 +3,34 @@
 var expect = require('chai').expect;
 var spawns = require('../');
 
-describe("spawns", function(){
-    it("", function(done){
-        spawns(['ls', 'ls'], {
-            stdio: 'inherit'
-        
-        }).on('close', function (code) {
-            expect(code).to.equal(0);
-            
-            done();
-        });
+describe("spawns", function() {
+  it("normal", function(done) {
+    spawns(['ls', 'ls'], {
+      stdio: 'inherit'
+
+    }).on('close', function(code) {
+      expect(code).to.equal(0);
+
+      done();
     });
+  });
+
+  var proto = spawns.Spawns.prototype;
+
+  // #4
+  var command = 'command -a "a \'b\' c" -b \'a "b" c\' -c "a b" -d \'a b\' -e "a" -f \'a\''
+  it("double quoted: " + command, function(){
+    var parsed = proto._parse_command(command);
+
+    expect(parsed.name).to.equal('command');
+    expect(parsed.args).to.deep.equal([
+      '-a', '"a \'b\' c"', 
+      '-b', "'a \"b\" c'", 
+      '-c', '"a b"',
+      '-d', "'a b'",
+      '-e', '"a"',
+      '-f', "'a'"
+    ]);
+    expect(parsed.origin).to.equal(command);
+  });
 });
